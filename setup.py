4d4e44878f04a6c3dc7d9b8cb2921689708b441f
Gabriele Calvo <gcalvo87@gmail.com>
2021-01-18 16:22:53 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     license='MIT license',
     zip_safe=False,
     keywords='azure iot edge dev tool',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <3.8',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
